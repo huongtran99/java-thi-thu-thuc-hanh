d87935c32114e50ada4f375168156d82d1f2bd8c
huongtran99 <huongtrannd1999@gmail.com>
2021-09-07 17:08:12 +0700

    first commit

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+import com.codegym.view.MenuProduct;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.run();
+    }
+}
